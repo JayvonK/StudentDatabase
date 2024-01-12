cff47cd5f4a68cff452265e0293e4186fb313ad2
JayvonK <jaykhuth@gmail.com>
2024-01-12 10:49:21 -0800

    connections

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,12 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using StudentDatabase.Model;
 
 namespace StudentDatabase.Data
 {
-    public class DataContext
+    public class DataContext : DbContext
     {
-        
+        public DataContext(DbContextOptions<DataContext> options) : base (options)
+        {
+            
+        }
+
+        public DbSet<Student> Students { get; set; }
     }
 }
